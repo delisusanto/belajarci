be19ba993fdb674db69c0336117990559415f682
delli <dhefil18@gmail.com>
2016-08-27 15:43:38 +0700

    isi sub halaman

---
+++ application/controllers/Halaman.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Halaman extends CI_Controller {
+
+
+	public function index()
+	{
+		echo "ini controller halaman";
+	}
+
+	public function tentang()
+	{
+		echo "ini halaman tentang";
+	}
+
+public function produk()
+	{
+		echo "ini halaman Produk";
+	}
+
+}
