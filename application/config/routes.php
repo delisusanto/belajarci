958d3d6c3954d7aa684a3399f89379910e97af78
delli <dhefil18@gmail.com>
2016-08-28 02:47:01 +0700

    Sudah sampai INSERT,UPDATE,DELETE

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['news/delete/(:any)'] = 'news/delete/$1';
+$route['news/update/(:any)'] = 'news/update/$1';
+$route['news/create'] = 'news/create';
 $route['news'] = 'news';
+$route['news/(:any)'] = 'news/view/$1';
 
 $route['default_controller'] = 'halaman/view';
 $route['(:any)'] = 'halaman/view/$1';
--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,4 +1,7 @@
 <?php foreach ($news as $news_item) { ?>
-	<h1><?php echo $news_item['title']; ?></h1>
+	<h1> <a href="<?php echo site_url('news/'.$news_item['slug']);?>"><?php echo $news_item['title']; ?>	 </a></h1>
 	<p><?php echo $news_item['text']; ?></p>
+	<a href="<?php echo site_url('news/update/'.$news_item['id']);?>">Edit</a>
+	<br>
+	<a href="<?php echo site_url('news/delete/'.$news_item['id']);?>">Hapus</a>
 <?php } ?>
